7cdc15661435e64ecd3834354adddfb4929d6a0a
chann44 <44chansw@gmail.com>
2025-12-11 13:21:13 +0530

    workflow working

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
+	"sort"
 
 	"github.com/chann44/http-0/internals"
 )
@@ -12,20 +14,19 @@ import (
 func main() {
 	cwd, err := os.Getwd()
 	if err != nil {
-		fmt.Printf("failed to start client", err)
-	}
-
-	cwd = filepath.Join(cwd, "requests")
-
-	requests, err := internals.GetFiles(cwd)
-	if err != nil {
-		fmt.Printf("Failed to process requests: %v\n", err)
+		fmt.Printf("failed to start client: %v\n", err)
 		return
 	}
 
-	var parsedRequests []internals.RequestDefinition
+	requestsDir := filepath.Join(cwd, "requests")
+	requestFiles, err := internals.GetFiles(requestsDir)
+	if err != nil {
+		fmt.Printf("Failed to get request files: %v\n", err)
+		return
+	}
 
-	for _, reqPath := range requests {
+	requestsMap := make(map[string]*internals.RequestDefinition)
+	for _, reqPath := range requestFiles {
 		reqBytes, err := internals.ReadFileToBytes(reqPath)
 		if err != nil {
 			fmt.Printf("Failed to read request file %s: %v\n", reqPath, err)
@@ -36,27 +37,177 @@ func main() {
 			fmt.Printf("Failed to parse request from %s: %v\n", reqPath, err)
 			continue
 		}
-		parsedRequests = append(parsedRequests, *parsedReq)
+		requestsMap[parsedReq.Name] = parsedReq
 	}
 
-	var requeestsToProcess []*http.Request
-
-	for _, rp := range parsedRequests {
-		req, err := internals.Build(&rp)
+	workflowsMap := make(map[string]*internals.Workflow)
+	workflowsDir := filepath.Join(cwd, "workflows")
+	if internals.DirExists(workflowsDir) {
+		workflowFiles, err := internals.GetFiles(workflowsDir)
 		if err != nil {
-			fmt.Printf("Failed to build request %s: %v\n", rp.Name, err)
-			continue
+			fmt.Printf("Failed to get workflow files: %v\n", err)
+			return
 		}
-		requeestsToProcess = append(requeestsToProcess, req)
+
+		for _, wfPath := range workflowFiles {
+			wfBytes, err := internals.ReadFileToBytes(wfPath)
+			if err != nil {
+				fmt.Printf("Failed to read workflow file %s: %v\n", wfPath, err)
+				continue
+			}
+			workflow, err := internals.ParseWorkFlows(wfBytes)
+			if err != nil {
+				fmt.Printf("Failed to parse workflow from %s: %v\n", wfPath, err)
+				continue
+			}
+			workflowsMap[workflow.Name] = workflow
+		}
+	}
+
+	flag.Usage = func() {
+		showHelp(requestsMap, workflowsMap)
+	}
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) == 0 {
+		showHelp(requestsMap, workflowsMap)
+		return
 	}
 
 	client := &http.Client{}
 
-	for _, req := range requeestsToProcess {
-		_, err := internals.ExecuteRequest(client, req)
-		if err != nil {
-			fmt.Printf("Failed to execute request: %v\n", err)
-			continue
+	command := args[0]
+
+	switch command {
+	case "request", "req", "r":
+		if len(args) < 2 {
+			fmt.Println("Error: request name is required")
+			fmt.Println("\nUsage: http-0 request <request-name>")
+			return
 		}
+		requestName := args[1]
+		executeRequest(requestName, requestsMap, client)
+
+	case "workflow", "wf", "w":
+		if len(args) < 2 {
+			fmt.Println("Error: workflow name is required")
+			fmt.Println("\nUsage: http-0 workflow <workflow-name>")
+			return
+		}
+		workflowName := args[1]
+		executeWorkflow(workflowName, workflowsMap, requestsMap, client)
+
+	default:
+		fmt.Printf("Error: unknown command '%s'\n", command)
+		fmt.Println("\nRun 'http-0' without arguments to see available commands.")
+	}
+}
+
+func showHelp(requestsMap map[string]*internals.RequestDefinition, workflowsMap map[string]*internals.Workflow) {
+	fmt.Println("http-0 - HTTP request and workflow executor")
+	fmt.Println("\nUsage:")
+	fmt.Println("  http-0 request <request-name>   Execute a single request")
+	fmt.Println("  http-0 workflow <workflow-name>  Execute a workflow")
+	fmt.Println("\nShort aliases:")
+	fmt.Println("  request: req, r")
+	fmt.Println("  workflow: wf, w")
+
+	// Show available requests
+	if len(requestsMap) > 0 {
+		fmt.Println("\nAvailable Requests:")
+		requestNames := make([]string, 0, len(requestsMap))
+		for name := range requestsMap {
+			requestNames = append(requestNames, name)
+		}
+		sort.Strings(requestNames)
+		for _, name := range requestNames {
+			req := requestsMap[name]
+			fmt.Printf("  - %s (%s %s)\n", name, req.Method, req.URL)
+		}
+	} else {
+		fmt.Println("\nNo requests found in ./requests directory")
+	}
+
+	if len(workflowsMap) > 0 {
+		fmt.Println("\nAvailable Workflows:")
+		workflowNames := make([]string, 0, len(workflowsMap))
+		for name := range workflowsMap {
+			workflowNames = append(workflowNames, name)
+		}
+		sort.Strings(workflowNames)
+		for _, name := range workflowNames {
+			wf := workflowsMap[name]
+			fmt.Printf("  - %s (%d steps)\n", name, len(wf.Steps))
+		}
+	} else {
+		fmt.Println("\nNo workflows found in ./workflows directory")
+	}
+
+	fmt.Println("\nExamples:")
+	if len(requestsMap) > 0 {
+		for name := range requestsMap {
+			fmt.Printf("  http-0 request %s\n", name)
+			break
+		}
+	}
+	if len(workflowsMap) > 0 {
+		for name := range workflowsMap {
+			fmt.Printf("  http-0 workflow %s\n", name)
+			break
+		}
+	}
+}
+
+func executeRequest(requestName string, requestsMap map[string]*internals.RequestDefinition, client *http.Client) {
+	requestDef, ok := requestsMap[requestName]
+	if !ok {
+		fmt.Printf("Error: request '%s' not found\n", requestName)
+		fmt.Println("\nAvailable requests:")
+		requestNames := make([]string, 0, len(requestsMap))
+		for name := range requestsMap {
+			requestNames = append(requestNames, name)
+		}
+		sort.Strings(requestNames)
+		for _, name := range requestNames {
+			fmt.Printf("  - %s\n", name)
+		}
+		return
+	}
+
+	fmt.Printf("Executing request: %s\n", requestName)
+	req, err := internals.Build(requestDef)
+	if err != nil {
+		fmt.Printf("Failed to build request: %v\n", err)
+		return
+	}
+
+	_, err = internals.ExecuteRequest(client, req)
+	if err != nil {
+		fmt.Printf("Failed to execute request: %v\n", err)
+		return
+	}
+}
+
+func executeWorkflow(workflowName string, workflowsMap map[string]*internals.Workflow, requestsMap map[string]*internals.RequestDefinition, client *http.Client) {
+	workflow, ok := workflowsMap[workflowName]
+	if !ok {
+		fmt.Printf("Error: workflow '%s' not found\n", workflowName)
+		fmt.Println("\nAvailable workflows:")
+		workflowNames := make([]string, 0, len(workflowsMap))
+		for name := range workflowsMap {
+			workflowNames = append(workflowNames, name)
+		}
+		sort.Strings(workflowNames)
+		for _, name := range workflowNames {
+			fmt.Printf("  - %s\n", name)
+		}
+		return
+	}
+
+	if err := internals.ExecuteWorkflows(workflow, requestsMap, client); err != nil {
+		fmt.Printf("Failed to execute workflow: %v\n", err)
+		return
 	}
 }
--- internals/workflows.go
+++ internals/workflows.go
@@ -1,1 +1,290 @@
 package internals
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+func ParseWorkFlows(workflowBytes []byte) (*Workflow, error) {
+	var workflowDef Workflow
+	if err := yaml.Unmarshal(workflowBytes, &workflowDef); err != nil {
+		return nil, err
+	}
+	return &workflowDef, nil
+}
+
+func ExtractNestedData(data interface{}, path string) (interface{}, error) {
+	if path == "" {
+		return data, nil
+	}
+
+	parts := strings.Split(path, "|")
+	current := data
+
+	for i, part := range parts {
+		part = strings.TrimSpace(part)
+		if part == "" {
+			continue
+		}
+
+		switch v := current.(type) {
+		case map[string]interface{}:
+			val, ok := v[part]
+			if !ok {
+				return nil, fmt.Errorf("key '%s' not found at path segment %d", part, i)
+			}
+			current = val
+
+		case []interface{}:
+			idx, err := strconv.Atoi(part)
+			if err != nil {
+				return nil, fmt.Errorf("expected array index at segment %d, got '%s'", i, part)
+			}
+			if idx < 0 || idx >= len(v) {
+				return nil, fmt.Errorf("array index %d out of bounds (length: %d)", idx, len(v))
+			}
+			current = v[idx]
+
+		default:
+			return nil, fmt.Errorf("cannot traverse path at segment %d: current value is not a map or array", i)
+		}
+	}
+
+	return current, nil
+}
+
+func SubstituteVariables(input string, ctx *WorkflowContext) (string, error) {
+	re := regexp.MustCompile(`\{\{([^}]+)\}\}`)
+
+	var lastErr error
+	result := re.ReplaceAllStringFunc(input, func(match string) string {
+		content := strings.TrimSpace(match[2 : len(match)-2])
+
+		if strings.Contains(content, "|") {
+			parts := strings.SplitN(content, "|", 2)
+			stepName := strings.TrimSpace(parts[0])
+			path := strings.TrimSpace(parts[1])
+
+			response, ok := ctx.Steps[stepName]
+			if !ok {
+				lastErr = fmt.Errorf("step '%s' not found in context", stepName)
+				return match
+			}
+
+			if response.Error != nil {
+				lastErr = fmt.Errorf("step '%s' had an error", stepName)
+				return match
+			}
+
+			value, err := ExtractNestedData(response.Body, path)
+			if err != nil {
+				lastErr = fmt.Errorf("failed to extract data from step '%s': %v", stepName, err)
+				return match
+			}
+
+			return fmt.Sprintf("%v", value)
+		} else {
+			if val, ok := ctx.Variables[content]; ok {
+				return fmt.Sprintf("%v", val)
+			}
+			if val, ok := ctx.Global[content]; ok {
+				return fmt.Sprintf("%v", val)
+			}
+			lastErr = fmt.Errorf("variable '%s' not found", content)
+			return match
+		}
+	})
+
+	return result, lastErr
+}
+
+func SubstituteInMap(m map[string]string, ctx *WorkflowContext) (map[string]string, error) {
+	result := make(map[string]string)
+	for k, v := range m {
+		substituted, err := SubstituteVariables(v, ctx)
+		if err != nil {
+			return nil, err
+		}
+		result[k] = substituted
+	}
+	return result, nil
+}
+
+func SubstituteInBody(body interface{}, ctx *WorkflowContext) (interface{}, error) {
+	if body == nil {
+		return nil, nil
+	}
+
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal body: %v", err)
+	}
+
+	bodyStr := string(bodyBytes)
+	substituted, err := SubstituteVariables(bodyStr, ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to substitute variables in body: %v", err)
+	}
+
+	var result interface{}
+	if err := json.Unmarshal([]byte(substituted), &result); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal substituted body: %v", err)
+	}
+
+	return result, nil
+}
+
+func ExecuteWorkflows(workflow *Workflow, requestsMap map[string]*RequestDefinition, client *http.Client) error {
+	ctx := &WorkflowContext{
+		Steps:     make(map[string]*Response),
+		Variables: workflow.Variables,
+		Global:    make(map[string]interface{}),
+	}
+
+	fmt.Printf("\n%s\n", strings.Repeat("=", 60))
+	fmt.Printf("WORKFLOW: %s\n", workflow.Name)
+	fmt.Printf("%s\n", strings.Repeat("=", 60))
+
+	for stepIdx, step := range workflow.Steps {
+		fmt.Printf("\n[Step %d/%d] %s\n", stepIdx+1, len(workflow.Steps), step.Name)
+		fmt.Println(strings.Repeat("-", 40))
+
+		requestDef, ok := requestsMap[step.Request]
+		if !ok {
+			return fmt.Errorf("request '%s' not found for step '%s'", step.Request, step.Name)
+		}
+
+		stepRequest := &RequestDefinition{
+			Name:    requestDef.Name,
+			Method:  requestDef.Method,
+			URL:     requestDef.URL,
+			Headers: make(map[string]string),
+			Query:   make(map[string]string),
+		}
+
+		for k, v := range requestDef.Headers {
+			stepRequest.Headers[k] = v
+		}
+		for k, v := range requestDef.Query {
+			stepRequest.Query[k] = v
+		}
+
+		if step.Headers != nil {
+			for k, v := range step.Headers {
+				stepRequest.Headers[k] = v
+			}
+		}
+		if step.Query != nil {
+			for k, v := range step.Query {
+				stepRequest.Query[k] = v
+			}
+		}
+
+		url, err := SubstituteVariables(stepRequest.URL, ctx)
+		if err != nil {
+			return fmt.Errorf("failed to substitute variables in URL for step '%s': %v", step.Name, err)
+		}
+		stepRequest.URL = url
+
+		stepRequest.Headers, err = SubstituteInMap(stepRequest.Headers, ctx)
+		if err != nil {
+			return fmt.Errorf("failed to substitute variables in headers for step '%s': %v", step.Name, err)
+		}
+
+		stepRequest.Query, err = SubstituteInMap(stepRequest.Query, ctx)
+		if err != nil {
+			return fmt.Errorf("failed to substitute variables in query for step '%s': %v", step.Name, err)
+		}
+
+		var bodyToUse interface{}
+		if step.Body != nil {
+			bodyToUse = step.Body
+		} else if requestDef.Body != nil {
+			bodyBytes, err := io.ReadAll(requestDef.Body)
+			if err == nil && len(bodyBytes) > 0 {
+				json.Unmarshal(bodyBytes, &bodyToUse)
+			}
+		}
+
+		if bodyToUse != nil {
+			substitutedBody, err := SubstituteInBody(bodyToUse, ctx)
+			if err != nil {
+				return fmt.Errorf("failed to substitute variables in body for step '%s': %v", step.Name, err)
+			}
+
+			bodyJSON, err := json.Marshal(substitutedBody)
+			if err != nil {
+				return fmt.Errorf("failed to marshal body for step '%s': %v", step.Name, err)
+			}
+			stepRequest.Body = bytes.NewReader(bodyJSON)
+
+			if _, ok := stepRequest.Headers["Content-Type"]; !ok {
+				stepRequest.Headers["Content-Type"] = "application/json"
+			}
+		}
+
+		httpRequest, err := Build(stepRequest)
+		if err != nil {
+			return fmt.Errorf("failed to build request for step '%s': %v", step.Name, err)
+		}
+
+		response, err := ExecuteRequest(client, httpRequest)
+		if err != nil {
+			fmt.Printf("ERROR in step '%s': %v\n", step.Name, err)
+		}
+
+		ctx.Steps[step.Name] = &response
+
+		if step.Extract != nil {
+			fmt.Println("\nExtracting variables:")
+			for varName, path := range step.Extract {
+				value, err := ExtractNestedData(response.Body, path)
+				if err != nil {
+					fmt.Printf("  Warning: failed to extract '%s': %v\n", varName, err)
+					continue
+				}
+				ctx.Variables[varName] = value
+				fmt.Printf("  %s = %v\n", varName, value)
+			}
+		}
+
+		if response.Error != nil && step.OnError != nil {
+			for _, cond := range step.OnError {
+				if cond.Stop {
+					return fmt.Errorf("workflow stopped due to error in step '%s': %v", step.Name, response.Error)
+				}
+				if cond.Goto != "" {
+					// TODO: Implement goto logic if needed
+					fmt.Printf("Warning: goto not yet implemented\n")
+				}
+			}
+		}
+
+		if response.Error == nil && step.OnSuccess != nil {
+			for _, cond := range step.OnSuccess {
+				if cond.Stop {
+					fmt.Printf("\nWorkflow completed successfully after step '%s'\n", step.Name)
+					return nil
+				}
+				if cond.Goto != "" {
+					// TODO: Implement goto logic if needed
+					fmt.Printf("Warning: goto not yet implemented\n")
+				}
+			}
+		}
+	}
+
+	fmt.Printf("\n%s\n", strings.Repeat("=", 60))
+	fmt.Printf("WORKFLOW COMPLETED: %s\n", workflow.Name)
+	fmt.Printf("%s\n\n", strings.Repeat("=", 60))
+
+	return nil
+}
